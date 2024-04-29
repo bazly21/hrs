541349caefcbef726bcfbab0ac4a9cbeabe0c41d
bazly21 <bazly21@gmail.com>
2024-04-29 15:57:30 +0800

    - Added tenant_criteria.dart

---
+++ lib/model/tenant_criteria/tenant_criteria.dart
@@ -0,0 +1,23 @@
+class TenantCriteria {
+  final String profileType;
+  final int numberOfPax;
+  final String nationality;
+  final String tenancyDuration;
+
+  TenantCriteria({
+    required this.profileType,
+    required this.numberOfPax,
+    required this.nationality,
+    required this.tenancyDuration,
+  });
+
+  // Convert TenantCriteria object to map
+  Map<String, dynamic> toMap() {
+    return {
+      'profileType': profileType,
+      'numberOfPax': numberOfPax,
+      'nationality': nationality,
+      'tenancyDuration': tenancyDuration,
+    };
+  }
+}
