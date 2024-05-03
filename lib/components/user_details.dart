e6fa99070cfdaa220328fbc00c9c0d656b3241a0
bazly21 <bazly21@gmail.com>
2024-05-03 22:56:19 +0800

    user_details.dart - Added user object - Added user log in checking when click on the chat button

--- lib/components/user_details.dart
+++ lib/components/user_details.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:hrs/pages/chat_page.dart';
+import 'package:hrs/pages/login_page.dart';
 import 'package:hrs/services/navigation/navigation_utils.dart';
 
 class UserDetails extends StatelessWidget {
@@ -9,7 +11,9 @@ class UserDetails extends StatelessWidget {
   final double rating;
   final VoidCallback? onPressed;
 
-  const UserDetails(
+  final User? user = FirebaseAuth.instance.currentUser;
+
+  UserDetails(
       {super.key,
       required this.landlordName,
       this.position = "Property Owner",
@@ -80,14 +84,23 @@ class UserDetails extends StatelessWidget {
 
         ElevatedButton(
           onPressed: () {
-            NavigationUtils.pushPage(
-              context,
-              ChatPage(
-                receiverID: landlordID,
-                receiverName: landlordName
-              ),
-              SlideDirection.left
-            ); // Go to chat page
+            if (user != null) {
+              NavigationUtils.pushPage(
+                  context,
+                  ChatPage(receiverID: landlordID, receiverName: landlordName),
+                  SlideDirection.left);
+            }
+            else {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text("Please log in to chat with the owner")),
+              );
+
+              NavigationUtils.pushPage(
+                context,
+                LoginPage(),
+                SlideDirection.left,
+              );
+            }
           },
           style: ButtonStyle(
             fixedSize: const MaterialStatePropertyAll(Size.fromHeight(42)),
@@ -137,19 +150,17 @@ class UserDetails extends StatelessWidget {
 
           // Rating value
           RichText(
-            text: TextSpan(
-              // Default text style
-              style: const TextStyle(color: Colors.black, fontSize: 14),
-              
-              children: [
-                TextSpan(text: "$rating"), 
+              text: TextSpan(
+                  // Default text style
+                  style: const TextStyle(color: Colors.black, fontSize: 14),
+                  children: [
+                TextSpan(text: "$rating"),
                 TextSpan(
-                  text: numReview > 1
-                      ? " ($numReview Reviews)"
-                      : " ($numReview Review)",
-                  style: const TextStyle(color: Color(0xFF7D7F88)))
-            ])
-          ),
+                    text: numReview > 1
+                        ? " ($numReview Reviews)"
+                        : " ($numReview Review)",
+                    style: const TextStyle(color: Color(0xFF7D7F88)))
+              ])),
         ],
       );
     }
