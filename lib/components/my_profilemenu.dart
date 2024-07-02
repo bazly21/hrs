e4fc9d2b844aea57937f1b6ff95c5f32834dd38d
bazly21 <bazly21@gmail.com>
2024-07-02 23:35:24 +0800

    my_profilemenu.dart - Changed IconButton to Icon

--- lib/components/my_profilemenu.dart
+++ lib/components/my_profilemenu.dart
@@ -5,56 +5,52 @@ class ProfileMenu extends StatelessWidget {
   final VoidCallback? onPressed;
   final IconData icon;
 
-  const ProfileMenu({
-    super.key,
-    required this.text,
-    this.onPressed,
-    required this.icon
-  });
+  const ProfileMenu(
+      {super.key, required this.text, this.onPressed, required this.icon});
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: onPressed ?? (){},
-      child: Row(
-        children: [
-          // Icon
-          Container(
-            padding: const EdgeInsets.all(8.0),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 3,
-                  offset: Offset(0, 2),
-                ),
-              ],
+      onTap: onPressed ?? () {},
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Row(
+          children: [
+            // Icon
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 3,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Icon(
+                icon,
+                size: 20,
+              ),
             ),
-            child: Icon(icon, size: 20,),
-          ),
 
-          // Add space between elements
-          const SizedBox(width: 15.0),
+            // Add space between elements
+            const SizedBox(width: 15.0),
 
-          // Menu Label
-          Text(
-            text,
-            style: const TextStyle(
-              fontWeight: FontWeight.w600,
-              fontSize: 16
+            // Menu Label
+            Text(
+              text,
+              style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
             ),
-          ),
 
-          const Spacer(),
+            const Spacer(),
 
-          // Arrow Icon
-          IconButton(
-            onPressed: (){}, 
-            icon: const Icon(Icons.arrow_forward)
-          )
-        ],
+            // Arrow Icon
+            const Icon(Icons.arrow_forward)
+          ],
+        ),
       ),
     );
   }
